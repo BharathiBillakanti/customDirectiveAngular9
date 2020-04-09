afbfed0e494cd64eef2aac3f854ace1a96da7407
Bharathi Billakanti <bbillaka@linkedin.com>
2020-04-09 16:16:51 +0530

    First commit

---
+++ src/app/counter.directive.ts
@@ -0,0 +1,25 @@
+import { Directive,ElementRef, HostListener, Input, Output, EventEmitter } from '@angular/core';
+
+@Directive({
+  selector: '[appCounter]'
+})
+export class CounterDirective {
+x=0;
+el:any;
+  constructor(private elem:ElementRef) {
+   }
+   ngOnInit() {
+    console.log(this.elem.nativeElement.innerText);
+    
+    this.x = document.querySelectorAll('.widget').length;
+    this.el = document.querySelectorAll('.count');
+    this.el[0].innerText = ' Widjet count is:' + this.x;
+  }
+  @Output('clickOutside') clickOutside: EventEmitter<any> = new EventEmitter();
+@HostListener('document:click', ['$event.target']) onMouseEnter(targetElement) {
+  if (targetElement.type == 'submit') {
+    this.el[0].innerText = ' Widjet count is:' + 1;
+  }
+}
+
+}
